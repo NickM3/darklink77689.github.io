7ac0151af2ea988dd2bee96bd5c8814807630760
Anirudh <s-asuresh@lwsd.org>
2018-10-21 16:32:45 -0700

    new stats section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,48 @@
 function closeAnnouncements() {
-    $('#announcements').slideUp("slow", function() {});
+    $('#announcements').slideUp("slow", function () { });
 }
 function closeWarning() {
-    $('#warning').slideUp("slow", function() {});
+    $('#warning').slideUp("slow", function () { });
 }
+
+var memCount = new CountUp("members", 0, 26, 0, 3);
+var robotCount = new CountUp("robots", 0, 9999, 0, 2000);
+var yearCount = new CountUp("years", 0, 4, 0, 3);
+var winCount = new CountUp("wins", 0, 1, 0, 3);
+var sponsorCount = new CountUp("sponsors", 0, 12, 0, 3);
+
+
+$(window).scroll(function() {
+    if ($('#members').visible()) {
+        if (!memCount.error) {
+            memCount.start();
+        } else {
+            console.error(memCount.error);
+        }
+
+
+        if (!robotCount.error) {
+            robotCount.start();
+        } else {
+            console.error(robotCount.error);
+        }
+
+        if (!yearCount.error) {
+            yearCount.start();
+        } else {
+            console.error(yearCount.error);
+        }
+
+        if (!winCount.error) {
+            winCount.start();
+        } else {
+            console.error(winCount.error);
+        }
+        
+        if (!sponsorCount.error) {
+            sponsorCount.start();
+        } else {
+            console.error(sponsorCount.error);
+        }
+    }
+});
