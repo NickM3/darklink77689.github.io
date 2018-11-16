165791205cc8616643ce3c6c3f8a63a9e095f9ba
Anirudh <s-asuresh@lwsd.org>
2018-11-15 22:36:50 -0800

    speed up loading

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+    var accordions = bulmaAccordion.attach();
+    $('.accordion').slideUp();
+});
+function bot16() {
+    if ($('#stronghold').visible()) {
+        $('.accordion').slideUp();
+    } else {
+        $('.accordion').slideUp();
+        $('#stronghold').slideToggle();
+    }
+}
+function bot17() {
+    if ($('#steamworks').visible()) {
+        $('.accordion').slideUp();
+    } else {
+        $('.accordion').slideUp();
+        $('#steamworks').slideToggle();
+    }
+}
+function bot18() {
+    if ($('#powerup').visible()) {
+        $('.accordion').slideUp();
+    } else {
+        $('.accordion').slideUp();
+        $('#powerup').slideToggle();
+    }
+}
 function closeAnnouncements() {
     $('#announcements').slideUp("slow", function () { });
 }
@@ -6,13 +34,13 @@ function closeWarning() {
 }
 
 var memCount = new CountUp("members", 0, 33, 0, 3);
-var robotCount = new CountUp("robots", 0, 4, 0, 3);
+var robotCount = new CountUp("robots", 0, 7, 0, 3);
 var yearCount = new CountUp("years", 0, 4, 0, 3);
 var winCount = new CountUp("wins", 0, 1, 0, 3);
 var sponsorCount = new CountUp("sponsors", 0, 12, 0, 3);
 
 
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($('#members').visible()) {
         if (!memCount.error) {
             memCount.start();
@@ -38,7 +66,7 @@ $(window).scroll(function() {
         } else {
             console.error(winCount.error);
         }
-        
+
         if (!sponsorCount.error) {
             sponsorCount.start();
         } else {
